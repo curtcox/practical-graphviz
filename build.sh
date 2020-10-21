709d421d1bf12a1bbfcd5d932d499ce8303725e6
Curt Cox <curt.cox@asynchrony.com>
2020-10-20 22:02:42 -0500

    Slide polish.

--- build.sh
+++ build.sh
@@ -11,9 +11,19 @@ for f in *.dot; do
   base=`echo "$f" | cut -d'.' -f1`
   { echo "@startuml"; cat $f; echo -e "\n@enduml"; } > puml/$base.puml
   sed -i '' 's/graph G {/graph G {\
-     bgcolor="transparent"\
-     node[style=filled color=white]\
+     STYLE_FOR_SLIDES\
      /g' puml/$base.puml
   dot -Tpng -o png/$base.png $f
   dot -Tsvg -o svg/$base.svg $f
+done
+
+sed -i '' 's/STYLE_FOR_SLIDES//g' puml/workflow.puml
+sed -i '' 's/STYLE_FOR_SLIDES//g' puml/links.puml
+
+# Style remaining as transparent
+for f in puml/*; do
+  sed -i '' 's/STYLE_FOR_SLIDES/\
+  bgcolor="transparent"\
+  node[style=filled color=white]\
+     /g' $f
 done
